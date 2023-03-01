cb16ce6087f3f309a559696c02864460270b450d
bekirbali <bfbali43@gmail.com>
2023-03-02 00:11:09 +0300

    add user form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const AddUser = () => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age(Years)</label>
-      <input type="number" name="age" id="" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age(Years)</label>
+        <input type="number" name="age" id="" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
