2766d10900f6d3a74b19bbc1cebf1f10cccf153d
bekirbali <bfbali43@gmail.com>
2023-03-01 23:19:43 +0300

    add user form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
   return (
     <div className="App">
       <h1>Adding User</h1>
+      <AddUser />
     </div>
   );
 }
